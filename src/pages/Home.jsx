47a0c65056695e4227a2212b5a209f98de047f17
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-04-20 10:39:52 +0530

    homepage posts working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,18 +31,18 @@ function Home() {
     }
 
     return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-               {posts.map((post) => {
-                 <div key={post.$id} className='p-2 w-1/4'>
-                     <PostCard {...post} />
-                 </div>
-               })}
-            </div>
-        </Container>
-    </div>
-  )
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Home
